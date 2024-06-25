ced3e5c59688e88f9da0603ddf86cfd574cdde9d
John Deck <jdeck88@gmail.com>
2024-06-25 17:52:27 -0400

    making new gulpfile with a build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 var gulp = require('gulp');
 var rimraf = require('rimraf');
 
-
-function defaultTask(cb) {
-    
-    cb()
-    gulp.src('app/*')    
-        .pipe(gulp.dest('public/'))
-    gulp.src('app/trait-viz/lib/*')    
-        .pipe(gulp.dest('public/trait-viz/lib/'))
-}
-  
-exports.default = defaultTask
-
+// Clean task to delete the 'public' directory
 gulp.task('clean', function(cb) {
     rimraf('./public', cb);
 });
+
+// Default task to copy files from 'app' to 'public'
+function defaultTask(cb) {
+    gulp.src('app/*')
+        .pipe(gulp.dest('public/'))
+        .on('end', function() {
+            gulp.src('app/trait-viz/lib/*')
+                .pipe(gulp.dest('public/trait-viz/lib/'))
+                .on('end', cb);
+        });
+}
+
+// Export default task
+exports.default = defaultTask;
+
+// Register default task as a named Gulp task
+gulp.task('default', defaultTask);
+
+// Build task to run 'clean' and then 'defaultTask'
+gulp.task('build', gulp.series('clean', defaultTask));
+
